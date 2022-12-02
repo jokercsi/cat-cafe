5170baa6275613f26f25e6840703ee77db63906f
jokercsi <jokercsi@gmail.com>
2022-12-02 15:37:01 +0900

    query builder

--- app/Http/Controllers/Admin/AdminBlogController.php
+++ app/Http/Controllers/Admin/AdminBlogController.php
@@ -14,7 +14,7 @@ class AdminBlogController extends Controller
     // 블로그 내용 화면
     public function index()
     {
-        $blogs = Blog::all();
+        $blogs = Blog::latest('updated_at')->limit(10)->get();
         return view('admin.blogs.index', ['blogs' => $blogs]);
     }
 

a0418e1bd5686afb7c754957c46bcf520465569c
jokercsi <jokercsi@gmail.com>
2022-12-07 18:30:26 +0900

    route group

--- routes/web.php
+++ routes/web.php
@@ -26,13 +26,25 @@ Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 Route::post('/contact', [ContactController::class, 'sendMail']);
 Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');
 
-// blog
-Route::get('/admin/blogs', [AdminBlogController::class, 'index'])->name('admin.blogs.index')->middleware('auth');
-Route::get('/admin/blogs/create', [AdminBlogController::class, 'create'])->name('admin.blogs.create');
-Route::post('/admin/blogs', [AdminBlogController::class, 'store'])->name('admin.blogs.store');
-Route::get('/admin/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('admin.blogs.edit');
-Route::put('/admin/blogs/{blog}', [AdminBlogController::class, 'update'])->name('admin.blogs.update');
-Route::delete('/admin/blogs/{blog}', [AdminBlogController::class, 'destroy'])->name('admin.blogs.destroy');
+
+// route 관리 (middleware)
+Route::prefix('/admin')
+    ->name('admin.')
+    ->middleware('auth')
+    ->group(function() {
+        // blog
+        // Route::get('/blogs', [AdminBlogController::class, 'index'])->name('blogs.index');   Route::get('/admin/blogs/create', [AdminBlogController::class, 'create'])->name('blogs.create');
+        // Route::post('/blogs', [AdminBlogController::class, 'store'])->name('blogs.store');
+        // Route::get('/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('blogs.edit');
+        // Route::put('/blogs/{blog}', [AdminBlogController::class, 'update'])->name('blogs.update');
+        // Route::delete('/blogs/{blog}', [AdminBlogController::class, 'destroy'])->name('blogs.destroy');
+        
+        Route::resource('/blogs', AdminBlogController::class)->except('show');
+        
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    });
+
+
 
 // 유저 관리
 Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
@@ -41,4 +53,3 @@ Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users
 //로그인
 Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
 Route::post('/admin/login', [AuthController::class, 'login']);
-Route::post('/admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
